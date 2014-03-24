046128c394bae2688f893b7ef85a386dce28a1b8
marisai <sai@marisai.com>
2014-03-24 19:25:58 +0800

    BackStage.Code Session => Session.data Fixed.

--- backstage/module/code.js
+++ backstage/module/code.js
@@ -7,12 +7,12 @@ exports.run=function(req,res){
 		res=null;
 		return;
 	}
-	if(!req.session['rouwan_path']){
-		req.session['rouwan_path']=__dirname;
+	if(!req.session.data['rouwan_path']){
+		req.session.data['rouwan_path']=__dirname;
 	}
 	rw.http.zout(
 		rw.t.render(
-			__dirname+'/../template/page.html',{'TITLE':'Code - ','LINK':'<script type="text/javascript" src="./js/code.js?sp=2"></script><script type="text/javascript" src="./js/ace/ace.js?sp=3"></script><link href="./css/code.css?sp=2" rel="stylesheet" type="text/css" />','MCUR2':' cur','WRAPPER':rw.t.render(__dirname+'/../template/code.html',{'PATH':req.session['rouwan_path']}),'V':rw.config.version}
+			__dirname+'/../template/page.html',{'TITLE':'Code - ','LINK':'<script type="text/javascript" src="./js/code.js?sp=2"></script><script type="text/javascript" src="./js/ace/ace.js?sp=3"></script><link href="./css/code.css?sp=2" rel="stylesheet" type="text/css" />','MCUR2':' cur','WRAPPER':rw.t.render(__dirname+'/../template/code.html',{'PATH':req.session.data['rouwan_path']}),'V':rw.config.version}
 		),
 		req,
 		res
