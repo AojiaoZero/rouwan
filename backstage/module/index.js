88a9ab751dccffca3c5da1e2db6f58637e451285
marisai <sai@marisai.com>
2014-03-30 12:38:34 +0800

    BackStage.timetag Added.

--- backstage/module/index.js
+++ backstage/module/index.js
@@ -16,9 +16,13 @@ exports.run=function(req,res){
 	lav[0]=lav[0].toFixed(2);
 	lav[1]=lav[1].toFixed(2);
 	lav[2]=lav[2].toFixed(2);
+	var tthi='';
+	if(!rw.config.backstage.switch.timetag){
+		tthi=' class="hide"';
+	}
 	rw.http.zout(
 		rw.t.render(
-			__dirname+'/../template/page.html',{'LINK':'<script type="text/javascript" src="./js/index.js?sp=2"></script><script type="text/javascript" src="./js/ace/ace.js?sp=3"></script><link href="./css/index.css?sp=2" rel="stylesheet" type="text/css" />','MCUR1':' cur','V':rw.config.version,'WRAPPER':rw.t.render(__dirname+'/../template/index.html',{'NV':process.version,'PID':process.pid,'TITLE':process.title,'MEM':parseInt(process.memoryUsage().rss/1024/1024)+' MB','MEMP':Math.ceil(process.memoryUsage().rss*100/os.totalmem())+'%','SMEM':parseInt(os.totalmem()/1024/1024)+' MB','UP':up,'LAV':lav.join(', '),'FRM':parseInt(os.freemem()/1024/1024)+' MB','OSUP':osup,'OID':'rw.session.'+req.session.sid})}
+			__dirname+'/../template/page.html',{'LINK':'<script type="text/javascript" src="./js/index.js?sp=2"></script><script type="text/javascript" src="./js/ace/ace.js?sp=3"></script><link href="./css/index.css?sp=2" rel="stylesheet" type="text/css" />','MCUR1':' cur','V':rw.config.version,'WRAPPER':rw.t.render(__dirname+'/../template/index.html',{'TTHI':tthi,'NV':process.version,'PID':process.pid,'TITLE':process.title,'MEM':parseInt(process.memoryUsage().rss/1024/1024)+' MB','MEMP':Math.ceil(process.memoryUsage().rss*100/os.totalmem())+'%','SMEM':parseInt(os.totalmem()/1024/1024)+' MB','UP':up,'LAV':lav.join(', '),'FRM':parseInt(os.freemem()/1024/1024)+' MB','OSUP':osup,'OID':'rw.session.'+req.session.sid})}
 		),
 		req,
 		res
